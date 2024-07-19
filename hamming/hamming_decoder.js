7703b2bae7d25c69e84d36f936b586f68e224f33
pabloozamora <pabloozamora02@gmail.com>
2024-07-18 21:06:42 -0600

    Algoritmo de corrección - Hamming

---
+++ hamming/hamming_decoder.js
@@ -0,0 +1,76 @@
+/*
+Universidad del Valle de Guatemala
+Redes
+Sección 10
+Laboratorio 2
+
+
+Autores:
+Diego Andrés Morales Aquino - 21762
+Pablo Andrés Zamora Vásquez - 21780
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function hammingDecode(encoded) {
+    let n = encoded.length;
+    let r = 0;
+
+    // Determinar los bits de paridad con los que cuenta la cadena codificada
+    while ((2 ** r - 1) < n) {
+        r += 1;
+    }
+
+    // Agregar un '0' al inicio para hacer que la cadena codificada sea 1-indexed
+    encoded = '0' + encoded;
+    let errorPos = 0;
+
+    // Validar bits de paridad
+    for (let i = 0; i < r; i++) { // Por cada bit de paridad
+        let parityIndex = 2 ** i; // Determinar su índice
+        let parityValue = 0; // Iniciar su valor de paridad en 0
+        for (let j = 1; j <= n; j++) { // Por cada bit en la secuencia codificada
+            if (j & parityIndex) { // Determinar si el bit j tiene un 1 en la posición que indica el indice de paridad
+                parityValue ^= parseInt(encoded[j]); // Calcular XOR de la paridad actual con el bit actual, determinando si se mantiene o se rompe la paridad
+            }
+        }
+        if (parityValue !== 0) { // Si el valor de paridad no es 0, hay un error
+            errorPos += parityIndex; // Agregar la posición del bit de paridad a la cuenta para calcular la posición del error
+        }
+    }
+
+    if (errorPos) {
+        console.log(`Bit incorrecto en la posición: ${errorPos}. Corrigiendo...`);
+        let encodedArray = encoded.split('');
+        encodedArray[errorPos] = encodedArray[errorPos] === '0' ? '1' : '0';
+        encoded = encodedArray.join('');
+    }
+
+    // Extraer los bits de data
+    let decoded = [];
+    for (let i = 1; i <= n; i++) {
+        if (!((i & (i - 1)) === 0)) { // Si i AND i-1 es 0, entonces i es una potencia de 2
+            decoded.push(encoded[i]); // Si no es potencia de 2, agregar al arreglo decodificado
+        }
+    }
+
+    return decoded.join('');
+}
+
+rl.question('Introduce una cadena de bits codificada: ', (encodedData) => {
+    // Introducir un error en el código codificado
+    encodedData = encodedData.split('');
+    encodedData[5] = encodedData[5] === '0' ? '1' : '0'; // Introduce un error en la posición 6 (índice 5)
+    encodedData = encodedData.join('');
+    console.log(`Datos codificados con error introducido: ${encodedData}`);
+
+    let decodedData = hammingDecode(encodedData);
+    console.log(`Datos decodificados: ${decodedData}`);
+
+    rl.close();
+});
---
+++ hamming/hamming_encoder.py
@@ -0,0 +1,46 @@
+'''
+Universidad del Valle de Guatemala
+Redes
+Sección 10
+Laboratorio 2
+
+
+Autores:
+Diego Andrés Morales Aquino - 21762
+Pablo Andrés Zamora Vásquez - 21780
+'''
+
+
+def hamming_encode(data):
+    """Codifica cadenas de bits usando código Hamming (n, m)"""
+    m = len(data)
+    r = 1
+    
+    # Determinar los bits de paridad necesarios
+    while (2**r - 1) < (m + r):
+        r += 1
+
+    n = m + r
+    encoded = ['0'] * (n + 1)
+
+    # Insertar bits de data en posiciones no potencia de 2
+    j = 0
+    for i in range(1, n + 1):
+        if not (i & (i-1)) == 0: # Si i AND i-1 es 0, entonces i es una potencia de 2
+            encoded[i] = data[j] # Insertar bit de data en la posición i
+            j += 1 # Pasar al siguiente bit de data
+
+    # Calcular bits de paridad
+    for i in range(r): # Por cada bit de paridad
+        parity_index = 2**i # Determinar su índice
+        parity_value = 0 # Su valor comienza en 0
+        for j in range(1, n + 1): # Por cada bit en la secuencia codificada
+            if j & parity_index: # Si j tiene un 1 en la posición que indica el indice de paridad, tomarlo en cuenta
+                parity_value ^= int(encoded[j]) # Calcular XOR de la paridad actual con el bit actual, determinando si se mantiene o se rompe la paridad
+        encoded[parity_index] = str(parity_value)
+
+    return ''.join(encoded[1:])
+
+data = input('Ingrese la cadena de bits a codificar: ')
+encoded_data = hamming_encode(data)
+print(f"Datos codificados: {encoded_data}")
